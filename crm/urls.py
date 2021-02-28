c7c14544637b26d9630b31dba6622bfbb2d59365
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-02-28 00:10:34 -0600

    adding url for exporting customer list

--- crm/urls.py
+++ crm/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     path('product/create/', views.product_new, name='product_new'),
     path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
     path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
+    url(r'^export/csv/$', views.export_customers, name='export_customers'),
 ]
 
